2c6abb08855984157be6189c3cc70cae3fe97fc1
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-30 17:06:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ submitData("John","john@gmail.com")
 })
 .then(json =>{
   let elem=document.createElement("div");
-  elem.innerHTML=js._id;
+  elem.innerHTML=js.id;
     document.body.appendChild(elem)
 }).catch(err=>{
     document.body.innerHTML=err.message;
