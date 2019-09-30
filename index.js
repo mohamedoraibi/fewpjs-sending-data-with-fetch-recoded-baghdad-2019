c0eeb173aab4476f145832ff9ee7e2af1c9b806d
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-30 17:17:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,22 @@
-// Add your code here
-let formData = {
-  dogName: "Byron",
-  dogBreed: "Poodle"
-};
-
-let configObj = {
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json"
-  },
-  method: "POST",
-  body: JSON.stringify(formData)
-};
-
-fetch("http://localhost:3000/dogs", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  });
-
-
 function submitData(name, email) {
-  let body=document.querySelector('body');
-  body=[];
-  fetch("http://localhost:3000/users", {
+  const URL = "http://localhost:3000/users";
+  const requestOptions = {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json"
     },
     method: "POST",
     body: JSON.stringify({ name: name, email: email })
-  }).then(function(response) {
-    return response.json();
-  }).then(function(object) {
-    let body=document.querySelector('body');
-    let node = document.createElement("li");  
-    let content=document.createTextNode(object.name+" "+object.email);
-    node.appendChild(content);
-    body.appendChild(node);
-    object.body.appendChild
-
-  }).catch(function(error) {
-    return error.message;
+  };
+  return fetch(URL, requestOptions) .then(res => {
+    return res.json();
+  })
+  .then(json => {
+    let elem = document.createElement("div");
+    elem.innerHTML = json.id;
+    document.body.appendChild(elem);
+  })
+  .catch(err => {
+    document.body.innerHTML = err.message;
   });
 }
-
