849eb2a011b73fe460681c678510e11057ccf007
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-30 17:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-
 function submitData(name, email) {
-    const URL='http://localhost:3000/users'
-    const requestOptions={
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },
-        method:"POST",
-        body: JSON.stringify({name: name, email:email})
-    }
-    return fetch(URL,requestOptions)
+  const URL = "http://localhost:3000/users";
+  const requestOptions = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({ name: name, email: email })
+  };
+  return fetch(URL, requestOptions) .then(res => {
+    return res.json();
+  })
+  .then(json => {
+    let elem = document.createElement("div");
+    elem.innerHTML = json.id;
+    document.body.appendChild(elem);
+  })
+  .catch(err => {
+    document.body.innerHTML = err.message;
+  });
 }
-
-submitData("John","john@gmail.com")
-.then(res=>{
-    return res.json()
-})
-.then(json =>{
-  let elem=document.createElement("div");
-  elem.innerHTML=json.id;
-    document.body.appendChild(elem)
-}).catch(err=>{
-    document.body.innerHTML=err.message;
-})
-    
